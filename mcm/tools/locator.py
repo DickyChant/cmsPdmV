de49e92e2583dfb72b8d004972e623208db5c0f4
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-03-17 10:57:17 +0100

    Enable to set some configurations via environment variables
    Environment variables
    ----------------------
    COUCH_CRED:  Path to a JSON file with the credentials to authenticate against CouchDB
    	     Required keys: {"username": <USERNAME>, "password": <PASSWORD>}
    USERCRT: Path to the VOMS certificate. The certificate must use the PEM format
    USERKEY: Path to the certificate private key. The key must use the PEM format
    CRED_FILE: Path to a JSON file with the service account credentials to open SSH connections
    	   Required keys: {"username": <USERNAME>, "password": <PASSWORD>}
    PRODUCTION: "True" if it is required to deploy the application as production environment
    
    Optional environment variables
    -------------------------------
    The following environment variables allow the user to override some default configurations
    available inside the tools/locator.py file
    
    COUCH_DATABASE_URL: Access URL to CouchDB
    LUCENE_URL: Access URL to Lucene engine for querying CouchDB
    BASE_URL: Application access URL. This is mainly used for components which send emails and notifications

--- mcm/tools/locator.py
+++ mcm/tools/locator.py
@@ -6,23 +6,24 @@ class locator:
         pass
 
     def isDev(self):
-        return not (self.isInt() or self.isProd())
-
-    def isInt(self):
-        host = os.environ['HOSTNAME']
-        return host in ['vocms082.cern.ch']  # Int machine
+        return not self.isProd()
 
     def isProd(self):
-        host = os.environ['HOSTNAME']
-        return host in ['vocms093.cern.ch', 'vocms0493.cern.ch']  # Prod machine
+        return os.getenv('PRODUCTION', '').lower() == 'true'
 
     def database_url(self):
+        database_url = os.getenv('COUCH_DATABASE_URL')
+        if database_url:
+            return database_url
         if self.isDev():
             return 'http://vocms0485.cern.ch:5984/'  # dev instance
         else:
             return 'http://vocms0490.cern.ch:5984/'  # prod instance
 
     def lucene_url(self):
+        lucene_url = os.getenv('LUCENE_URL')
+        if lucene_url:
+            return lucene_url
         if self.isDev():
             return 'http://vocms0485.cern.ch:5985/'  # dev instance
         else:
@@ -31,15 +32,15 @@ class locator:
     def workLocation(self):
         if self.isDev():
             return '/afs/cern.ch/cms/PPD/PdmV/work/McM/dev-submit/'
-            # legacy directory return '/afs/cern.ch/cms/PPD/PdmV/tools/prep2/prep2_submit_area/'
         else:
             return '/afs/cern.ch/cms/PPD/PdmV/work/McM/submit/'
 
     def baseurl(self):
+        base_url = os.getenv('BASE_URL')
+        if base_url:
+            return base_url
         if self.isDev():
             return 'https://cms-pdmv-dev.cern.ch/mcm/'
-        elif self.isInt():
-            return 'https://cms-pdmv-int.cern.ch/mcm/'
         else:
             return 'https://cms-pdmv.cern.ch/mcm/'
 
